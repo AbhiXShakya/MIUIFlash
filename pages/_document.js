4a3e320330a7305b7ebc2cd9f2b7a9737a937fc8
AbhiXShakya <abhixshakya@gmail.com>
2022-06-25 23:32:35 +0530

    initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="googlebot" content="noindex" />
+      </Head>
       <body className="font-metro">
         <Main />
         <NextScript />
